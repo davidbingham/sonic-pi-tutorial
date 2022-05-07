ef97683beceae28b80b76a98cc79c0624627a4cf
davidbingham <david@bingham.me>
2022-05-07 17:14:45 -0600

    added a little choir dealio

--- 1.1.rb
+++ 1.1.rb
@@ -1,6 +1,13 @@
 # Welcome to Sonic Pi
 
 live_loop :flibble do
+  
+  live_loop :flibble do
+    sample :ambi_choir, rate: 0.3
+    sample :bd_haus, rate: 1
+    sleep 1
+  end
+  
   sample :bd_haus, rate: 1
   sleep 1
 end
